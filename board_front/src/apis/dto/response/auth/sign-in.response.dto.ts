1dd3343e15598cea6a9e38091a5ea1fa2c3b8d88
devgiants75 <devgiants75@naver.com>
2025-05-20 10:37:48 +0900

    20250520 board_front settings

---
+++ board_front/src/apis/dto/response/auth/sign-in.response.dto.ts
@@ -0,0 +1,8 @@
+// sign-in.response.dto.ts
+
+import { ResponseDto } from '@/apis/dto/response';
+
+export default interface SignInResponseDto extends ResponseDto {
+  accessToken: string; // 토큰 정보
+  expiration: number; // 만료 시간
+}
